4bda311044b33008360dda228e5c52ff77603143
Kelver <kelver_kmv@hotmail.com>
2022-09-21 20:15:25 -0300

    chore: scripts de paginação, envio e melhoria na leitura de mensagens.

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -11,36 +11,71 @@ import { ApiService } from '../services/api.service';
 })
 export class PaginaComponent implements OnInit {
   public apiGreeting: string;
+  public dateTime: string;
+  public messages: object = [];
+  public currentPage: number = 1;
+  public nextPage: number = 1;
+  public lastPage: number;
+  public isPaginate: boolean = false;
 
   constructor(
     private apiService: ApiService
   ) { }
 
   ngOnInit(): void {
-    this.apiService.getHello().pipe(
+    this.loadMessages();
+  }
+
+  sendMessage(message: string): void {
+    this.apiService.storeNewMessage(message).pipe(
       catchError((err) => {
         this.apiGreeting = 'Falha na comunicação com o servidor.';
         return [];
       })
     ).subscribe((response) => {
       if (response) {
-        this.apiGreeting = response.mensagem;
+        (<HTMLFormElement>document.getElementById("addMessage")).reset();
+        return this.loadMessages();
       }
     });
   }
 
-  sendApi(message: string): void {
-    console.log(message);
-    this.apiService.sendApi(message).pipe(
+  checkData(): void {
+    this.apiService.getDateTime().pipe(
       catchError((err) => {
-        this.apiGreeting = 'Falha na comunicação com o servidor.';
+        this.dateTime = 'Falha na comunicação com o servidor.';
         return [];
       })
     ).subscribe((response) => {
       if (response) {
-        this.apiGreeting = response.mensagem;
+        console.log(response.data);
+        this.dateTime = response.data;
       }
     });
   }
 
+  loadMessages(): void {
+    this.nextPage = this.isPaginate ? this.currentPage + 1 : 1;
+    this.apiService.getAllMessages(this.nextPage).pipe(
+      catchError((err) => {
+        return 'Falha na comunicação com o servidor.';
+      })
+    ).subscribe((response) => {
+      if (response) {
+        this.currentPage = response.meta.current_page;
+        this.lastPage = response.meta.last_page;
+
+        this.checkData();
+        let messages = this.messages;
+        // @ts-ignore
+        this.messages = this.isPaginate ? messages.concat(response.data) : response.data;
+        this.isPaginate = false;
+      }
+    });
+  }
+
+  showMoreMessages() {
+    this.isPaginate = true;
+    this.loadMessages();
+  }
 }
